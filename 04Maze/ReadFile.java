cf953ef4ff06443b6daa777a79242f4d380d7d66
Winnie Chen <wchen10@stuy.edu>
2017-02-28 15:25:52 -0500

    Needed: fix and remove

--- 04Maze/ReadFile.java
+++ 04Maze/ReadFile.java
@@ -3,14 +3,22 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ReadFile {
-    public static void main(String args[]) throws FileNotFoundException {
-        //instead of a try/catch, you can throw the FileNotFoundException.
-        File infile = new File("input.txt");// can be a path"/full/path/to/file.txt" 
-        Scanner inf = new Scanner(text);
-        int lineNumber = 1;
-        while(inf.hasNextLine()){
-            String line = inf.nextLine();
+    public static void main(String args[]) throws FileNotFoundException{
+        File text = new File("input.txt");
+        Scanner in = new Scanner(text);
+        //int lineNumber = 1;
+        while(in.hasNextLine()){
+            String line = in.nextLine();
             System.out.println(line);
-        }       
+        }
+
+	//Alt
+	/*Scanner in = new Scanner(new File("WordList.txt"));
+	    while(in.hasNext()){
+		String word = in.next();
+		String wordUpperCase = word.toUpperCase();
+		dictWordList.add(wordUpperCase);
+	    }
+	*/
     }   
 }
