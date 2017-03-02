2056bf1e4fb344c283370141dcc7da0d87ba050a
Winnie Chen <wchen10@stuy.edu>
2017-03-02 15:07:53 -0500

    Completed: wrote and tested own code using Scanner. Needed: implement concept in Maze.java and delete this file

--- 04Maze/ReadFile.java
+++ 04Maze/ReadFile.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String args[]) throws FileNotFoundException{
-        File text = new File("input.txt");
+	Scanner input = new Scanner(new File("data1.dat"));
+	while(input.hasNextLine()){
+	    System.out.println(input.nextLine());
+	}
+	
+        //Mr. K's
+	/*File text = new File("input.txt");
         Scanner in = new Scanner(text);
         //int lineNumber = 1;
         while(in.hasNextLine()){
             String line = in.nextLine();
             System.out.println(line);
         }
+	*/
 
 	//Alt
 	/*Scanner in = new Scanner(new File("WordList.txt"));
