c9cc2d23159ced61cdb87321237a38e4cd0de2a1
Winnie Chen <wchen10@stuy.edu>
2017-03-03 15:10:14 -0500

    Completed: added if statement in the helper for finding square root in the case of n = 0. Needed: solve it another way so it feels less cheap

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -4,11 +4,16 @@ public class Recursion{
     }
 
     private static double sqrtHelper(double n, double guess){
-	double error = Math.abs(Math.sqrt(n) - guess) / Math.sqrt(n);
-	if(error < 0.00001){
-	    return guess;
+	if(n != 0.0){
+	    double error = Math.abs(Math.sqrt(n) - guess) / Math.sqrt(n);
+	    //For 0, the problem is division by 0...?
+	    if(error < 0.00001){
+		return guess;
+	    }else{
+		return sqrtHelper(n, (n / guess + guess) / 2);
+	    }
 	}else{
-	    return sqrtHelper(n, (n / guess + guess) / 2);
+	    return 0.0;
 	}
     }
     
@@ -26,5 +31,15 @@ public class Recursion{
 	System.out.println(sqrt(25));
 	System.out.println(sqrt(400));
 	System.out.println(sqrt(1000));
+	//1.0,2.0,4.0,7.0,10.0,100.0,1024.0,-1.0,0.0
+	System.out.println(sqrt(1.0));
+	System.out.println(sqrt(2.0));
+	System.out.println(sqrt(4.0));
+	System.out.println(sqrt(7.0));
+	System.out.println(sqrt(10.0));
+	System.out.println(sqrt(100.0));
+	System.out.println(sqrt(1024.0));
+	//System.out.println(sqrt(-1.0));
+	System.out.println(sqrt(0.0));
     }
 }
