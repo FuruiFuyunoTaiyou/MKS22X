415ed75b21c1b20c9eef9afdbf581258eb201d8f
Winnie Chen <wchen10@stuy.edu>
2017-03-07 15:36:16 -0500

    from pair programming

---
+++ 05USACO/USACO.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class USACO{
+    public USACO(){}
+
+    public static int bronze(String filename) throws FileNotFoundException{
+	return BronzeMar08.runBronze(filename);
+    }
+
+   /* public static void main(String[] args) throws FileNotFoundException{
+	for(int num = 1; num < 11; num++){
+	    System.out.println(bronze("makelake." + num + ".in"));
+	}
+    }*/
+}
