7e71e05d4fc1858347fda0e2a5455eec47d90694
Winnie Chen <wchen10@stuy.edu>
2017-03-12 17:57:10 -0400

    Completed: silver wrapper class, testing

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -7,9 +7,16 @@ public class USACO{
 	return BronzeMar08.runBronze(filename);
     }
 
-   public static void main(String[] args) throws FileNotFoundException{
+    public static int silver(String filename) throws FileNotFoundException{
+	return SilverMar08.runSilver(filename);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException{
        /*for(int num = 1; num < 11; num++){
 	    System.out.println(bronze("makelake." + num + ".in"));
+	    }
+	for(int x = 1; x <= 10; x++){
+	    System.out.println(silver("ctravel." + x + ".in"));
 	    }*/
    }
 }
