763e74808ecee50b7ab1000d7bd9d0de6070e306
Winnie Chen <wchen10@stuy.edu>
2017-03-15 06:31:32 -0400

    Completed: tested more. Optional: more efficient code

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -20,12 +20,19 @@ public class Quick{
 
 
     public static void main(String[] args){
-	int[]ary = {2, 10, 15, 23, 0, 5};
+	/*int[] ary = {2, 10, 15, 23, 0, 5};
 	System.out.println(quickselect( ary , 0 ) + " should return 0");
 	System.out.println(quickselect( ary , 1 ) + " should return 2");
 	System.out.println(quickselect( ary , 2 ) + " should return 5");
 	System.out.println(quickselect( ary , 3 ) + " should return 10");
 	System.out.println(quickselect( ary , 4 ) + " should return 15");
-	System.out.println(quickselect( ary , 5 ) + " should return 23");
+	System.out.println(quickselect( ary , 5 ) + " should return 23");*/
+	/*int[] test0 = {111, 999, 999, -888, 333, 0, 00, -22, 222, 555, 1001, 5, 3, 666, 7777, 1000000, 20000, -333, 222};
+	for(int i = 0; i < test0.length; i++){
+	    System.out.println(i + "th smallest: " + quickselect(test0, i));
+	    }*/
+	/*int[] test1 = {0};
+	int[] test2 = {};
+	System.out.println("0th smallest: " + quickselect(test1, 0));*/
     }
 }
