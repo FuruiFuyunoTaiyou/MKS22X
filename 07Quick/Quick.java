96407a2c47ba36b472dfb4925d6bcdcdff19e069
Winnie Chen <wchen10@stuy.edu>
2017-03-23 19:13:57 -0400

    Completed: quicksort, some manual testing. Needed: random testing

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -31,15 +31,24 @@ public class Quick{
 	}
     }
 
-    /*
+    
     public static void quicksort(int[] a){
-	
+	qsh(a, 0, a.length - 1);
     }
-    */
+
+    private static void qsh(int[] ary, int start, int end){
+	if(end > start){ //check condition
+	    int[] pivotIs = Partition.part3way(ary, start, end);
+	    qsh(ary, start, pivotIs[0] - 1);
+	    qsh(ary, pivotIs[1] + 1, end);
+	}
+    }
+    
 
 
 
     public static void main(String[] args){
+	/*
 	int[] ary = {2, 10, 15, 23, 0, 5};
 	System.out.println(quickselect( ary , 0 ) + " should return 0");
 	System.out.println(quickselect( ary , 1 ) + " should return 2");
@@ -56,5 +65,13 @@ public class Quick{
 	int[] test1 = {0};
 	int[] test2 = {};
 	System.out.println("0th smallest: " + quickselect(test1, 0));
+	*/
+
+	//int[] test3 = {111, 999, 999, -888, 333, 0, 00, -22, 222, 555, 1001, 5, 3, 666, 7777, 1000000, 20000, -333, 222};
+	int[] test3 = new int[1000];
+	quicksort(test3);
+	for(int i : test3){
+	    System.out.print(i + " ");
+	}
     }
 }
