7b70bd00f02ecb4781ff2ecc0ba70c2b8a52907a
Winnie Chen <wchen10@stuy.edu>
2017-04-20 06:39:54 -0400

    Needed: everything and fixing for compilation

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,3 +1,69 @@
-public class MyDeque{
+import java.util.*;
 
+public class MyDeque implements Deque<String>{
+    private String[] arr;
+    private int first;
+    private int last;
+
+    public MyDeque(){
+	arr = new String[10];
+	first = 0;
+	last = 0;
+    }
+
+    public int size(){
+	return last - first; //change according to wrapping
+    }
+
+    public void addFirst(String ele){
+	
+    }
+
+    public void addLast(String ele){
+
+    }
+
+    public String removeFirst(){
+
+    }
+
+    public String removeLast(){
+
+    }
+
+    public String getFirst(){
+
+    }
+
+    public String getLast(){
+
+    }
+
+    public Iterator<String> iterator(){
+
+    }
+
+    public Iterator<String> descendingIterator(){
+	
+    }
+
+    public String toString(){
+	String printVer = "[";
+	if(arr.length > 0){
+	    //
+	    printVer += " ";
+	    for(String str : arr){
+		printVer += ", " + str;
+	    }
+	}
+	printVer += "]";
+	return printVer;
+    }
+
+
+
+    public static void main(String[] args){
+	MyDeque test0 = new MyDeque();
+	System.out.println(test0);
+    }
 }
