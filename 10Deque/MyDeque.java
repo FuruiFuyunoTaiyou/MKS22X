e1b276e4d92711c1d21e8940da044e7a355cf872
Winnie Chen <wchen10@stuy.edu>
2017-04-21 05:39:35 -0400

    Completed: addLast, little testing. Needed: other methods, testing addLast with other methods to make sure it really works as intended

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-public class MyDeque implements Deque<String>{
+//public class MyDeque implements Deque<String>{
+public class MyDeque{
     private String[] arr;
     private int first;
     private int last;
@@ -11,46 +12,60 @@ public class MyDeque implements Deque<String>{
 	last = 0;
     }
 
-    public int size(){
-	return last - first; //change according to wrapping
+    private String[] expand(String[] original){
+	String[] copy = new String[original.length * 2];
+	int i = 0;
+	for(String ele : original){
+	    copy[i] = ele;
+	    i++;
+	}
+	return copy;
     }
 
     public void addFirst(String ele){
-	
+	if(ele != null){
+
+	}
     }
 
     public void addLast(String ele){
-
+	if(ele != null){
+	    if(arr[last] == null){
+		arr[first] = ele;
+	    }else if(arr[(last + 1) % arr.length] == null){
+		arr[last + 1] = ele;
+		last++;
+	    }else{
+		arr = expand(arr);
+		addLast(ele);
+	    }
+	}
     }
 
     public String removeFirst(){
 
+	return "HONMAKA";
     }
 
     public String removeLast(){
 
+	return "HONMAKA";
     }
 
     public String getFirst(){
 
+	return "HONMAKA";
     }
 
     public String getLast(){
 
-    }
-
-    public Iterator<String> iterator(){
-
-    }
-
-    public Iterator<String> descendingIterator(){
-	
+	return "HONMAKA";
     }
 
     public String toString(){
 	String printVer = "[";
+	//INSTEAD OF LENGTH, CALCULATE FROM FIRST - LAST ----------------------
 	if(arr.length > 0){
-	    //
 	    printVer += " ";
 	    for(String str : arr){
 		printVer += ", " + str;
@@ -65,5 +80,29 @@ public class MyDeque implements Deque<String>{
     public static void main(String[] args){
 	MyDeque test0 = new MyDeque();
 	System.out.println(test0);
+	test0.addLast("A");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("B");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("C");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("D");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("E");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("F");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("G");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("H");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("I");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("J");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("K");
+	System.out.println(test0.last + ":" + test0);
+	test0.addLast("L");
+	System.out.println(test0.last + ":" + test0);
     }
 }
