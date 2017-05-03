b6b14f07d372f20bac390a0994b0c771705fbe55
Winnie Chen <wchen10@stuy.edu>
2017-05-03 15:35:39 -0400

    Completed: method signatures. Needed: implementation and testing

---
+++ 13/MyHeap.java
@@ -0,0 +1,47 @@
+public class MyHeap{
+    private int constant;
+    private int size;
+
+
+    public MyHeap(){
+
+    }
+
+    public MyHeap(boolean max){
+
+    }
+
+    private int childLI(int nodeI){
+
+    }
+
+    private int childRI(int nodeI){
+
+    }
+
+    private int parent(int nodeI){
+
+    }
+
+    private void pushUp(){
+
+    }
+
+    private void pushDown(){
+
+    }
+
+    public void add(String s){
+
+    }
+
+    public String remove(){
+
+    }
+
+    public String peek(){
+
+    }
+
+
+}
