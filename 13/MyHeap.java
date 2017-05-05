55948625c45090e84d5cd3344c4fd1f997d8e17e
Winnie Chen <wchen10@stuy.edu>
2017-05-04 23:59:34 -0400

    Completed: pushUp, add. Needed: pushDown, remove, peek, testing

--- 13/MyHeap.java
+++ 13/MyHeap.java
@@ -21,14 +21,14 @@ public class MyHeap{
     }
 
     private void resize(){
-	String[] copy = new String[original.length * 2];
+	String[] copy = new String[heap.length * 2];
 	for(int i = 1; i < heap.length; i++){
 	    copy[i] = heap[i];
 	}
 	heap = copy;
     }
 
-    private int childLI(int nodeI){
+    private int childLI(int nodeI){ //what if the array is not large enough for children? or the node has no parent? I think it might be better to handle invalid values before/when using it.
 	return 2 * nodeI;
     }
 
@@ -47,12 +47,15 @@ public class MyHeap{
     }
 
     private void pushUp(int nodeI){ //recursive, unless you want to modify nodeI
-	if(heap[nodeI].compareTo(parent(nodeI)) * constant > 0){
-
+	//only node w/o parent is the one with index 1 or when parent i is 0
+	if(nodeI != 1 && heap[nodeI].compareTo(heap[parent(nodeI)]) * constant > 0){
+	    swap(nodeI, parent(nodeI));
+	    pushUp(parent(nodeI));
 	}
     }
 
     private void pushDown(int nodeI){
+	//only node w/o child on left or right is if... childLI or childRI is > size
 
     }
 
@@ -63,14 +66,19 @@ public class MyHeap{
 	}
 	heap[size] = s;
 	pushUp(size);
+	//---------------------------------------------------------------------
+	System.out.println(this);
+	//
     }
 
     public String remove(){
 
+	return "Vale!";
     }
 
     public String peek(){
 
+	return "Vale!";
     }
 
     public String toString(){
@@ -87,6 +95,20 @@ public class MyHeap{
 
 
     public static void main(String[] args){
-
+	MyHeap test = new MyHeap();
+	System.out.println(test);
+	test.add("a");
+	test.add("b");
+	test.add("abc");
+	test.add("c");
+	test.add("d");
+	test.add("e");
+	test.add("a");
+	test.add("b");
+	test.add("abc");
+	test.add("c");
+	test.add("d");
+	test.add("e");
+	
     }
 }
