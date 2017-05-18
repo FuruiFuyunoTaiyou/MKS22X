3b6952e05f67beb42bd4e497a6d4e4dc0a9c0628
Winnie Chen <wchen10@stuy.edu>
2017-05-18 15:05:48 -0400

    making sure committed

--- NotDue/FrontierPriorityQueue.java
+++ NotDue/FrontierPriorityQueue.java
@@ -1,6 +1,18 @@
 public class FrontierPriorityQueue implements Frontier{
     
 
+    public void add(Location lugar){
+	
+    }
+
+    public Location next{
+
+    }
+
+    public boolean hasNext(){
+
+    }
+
 
     public static void main(String[] args){
 
