8c4e323b4af7c2f36d288bf116cdb946da45bc27
Winnie Chen <wchen10@stuy.edu>
2017-05-24 06:24:28 -0400

    finished helper functions, testing with sample mazes. Needed: testing in open fields, testing animation

--- 15/MazeSolver.java
+++ 15/MazeSolver.java
@@ -1,4 +1,5 @@
 import java.lang.Math;
+import java.util.ArrayList;
 
 
 public class MazeSolver{
@@ -15,26 +16,26 @@ public class MazeSolver{
 	this.animate = animate; //not so sure what to do with/for animating yet
     }
 
-    /*
-    private int calcDistToStart(Location lugar){ //CAN count back using previous, but I doubt that's efficient
-        
-    }
-    */
-
     private int calcDistToGoal(int r, int c){
 	Location end = board.getEnd();
 	return Math.abs(r - end.getRow()) + Math.abs(c - end.getCol());
     }
 
-    private Location[] getValidNeighbors(Location lugar){ //-------------------
-	Location[] possible = {};
-	
+    private ArrayList<Location> getValidNeighbors(Location lugar){ //----------
+	int[] moves = {-1, 0, 0, 1, 1, 0, 0, -1};
+	ArrayList<Location> possible = new ArrayList<Location>();
+	for(int iMoveRow = 0, iMoveCol = 1; iMoveRow < moves.length - 1; iMoveRow += 2, iMoveCol += 2){
+	    int moveRow = moves[iMoveRow];
+	    int moveCol = moves[iMoveCol];
+	    if(board.get(lugar.getRow() + moveRow, lugar.getCol() + moveCol) == ' '){
+		possible.add(new Location(lugar.getRow() + moveRow, lugar.getCol() + moveCol, lugar, lugar.getDistToStart() + 1, calcDistToGoal(lugar.getRow() + moveRow, lugar.getCol() + moveCol), lugar.getAStar()));
+	    }
+	}
 	return possible;
     }
 
-    private boolean isEnd(Location lugar){ //----------------------------------
-
-	return false;
+    private boolean isEnd(Location lugar){ 
+	return lugar.getRow() == board.getEnd().getRow() && lugar.getCol() == board.getEnd().getCol();
     }
     
     public void solve(){
@@ -42,50 +43,74 @@ public class MazeSolver{
     }
 
     public void solve(int style){
-	Frontier frontier;
-	int distTraveled = 0; 
+	Frontier frontier = new FrontierStack(); //error if style not 0 - 3?
+	Location mazeStart = board.getStart();
 	if(style == 0){
 	    frontier = new FrontierStack();
-	    frontier.add(new Location(getStart().getRow(), getStart().getCol(), null, distTraveled, calcDistToGoal(getStart().getRow(), getStart().getCol()), false));
+	    frontier.add(new Location(mazeStart.getRow(), mazeStart.getCol(), null, 0, calcDistToGoal(mazeStart.getRow(), mazeStart.getCol()), false));
 	}else if(style == 1){
 	    frontier = new FrontierQueue();
-	    frontier.add(new Location(getStart().getRow(), getStart().getCol(), null, distTraveled, calcDistToGoal(getStart().getRow(), getStart().getCol()), false));
+	    frontier.add(new Location(mazeStart.getRow(), mazeStart.getCol(), null, 0, calcDistToGoal(mazeStart.getRow(), mazeStart.getCol()), false));
 	}else if(style == 2){
 	    frontier = new FrontierPriorityQueue();
-	    frontier.add(new Location(getStart().getRow(), getStart().getCol(), null, distTraveled, calcDistToGoal(getStart().getRow(), getStart().getCol()), false));
+	    frontier.add(new Location(mazeStart.getRow(), mazeStart.getCol(), null, 0, calcDistToGoal(mazeStart.getRow(), mazeStart.getCol()), false));
 	}else if(style == 3){
 	    frontier = new FrontierPriorityQueue(); 
-	    frontier.add(new Location(getStart().getRow(), getStart().getCol(), null, distTraveled, calcDistToGoal(getStart().getRow(), getStart().getCol()), true));
+	    frontier.add(new Location(mazeStart.getRow(), mazeStart.getCol(), null, 0, calcDistToGoal(mazeStart.getRow(), mazeStart.getCol()), true));
 	}
 	while(frontier.hasNext()){ //Will it stop if board searched?-----------
+	    //Yes, if there's nothing added, there won't be a frontier.next()--
 	    Location TBExplored = frontier.next();
-	    if(!isEnd(TExplored)){
+	    if(!isEnd(TBExplored)){
 		board.set(TBExplored.getRow(), TBExplored.getCol(), '.');
-		for(Location lugar : getValidNeighbors(TBExplored)){
+		for(Location lugar : getValidNeighbors(TBExplored)){ 
 		    frontier.add(lugar);
 		    board.set(lugar.getRow(), lugar.getCol(), '?');
 		}		     
 	    }else{
 		Location pathPart = TBExplored;
-		while(pathPart.getPrevious().getRow() != board.getStart.getRow() && pathPart.getPrevious().getCol() != board.getStart().getCol()){
+		board.set(TBExplored.getRow(), TBExplored.getCol(), 'E');
+		while(pathPart.getPrevious().getRow() != mazeStart.getRow() && pathPart.getPrevious().getCol() != mazeStart.getCol()){
 		    pathPart = pathPart.getPrevious();
 		    board.set(pathPart.getRow(), pathPart.getCol(), '@');
 		}
-		
+		//-------------------going back to start-----------------------
+		pathPart = pathPart.getPrevious();
+		board.set(pathPart.getRow(),pathPart.getCol(), 'S');
 	    }
 	}
+	//--------------------------what to do at end?-------------------------
+	
     }
 
     public String toString(){
 	if(animate){
 	    return board.toString(10);
 	}else{
-	    return board;
+	    return board.toString();
 	}
     }
 
     public static void main(String[] args){
-	MazeSolver test = new MazeSolver();
-	
+	MazeSolver test = new MazeSolver("data1.txt");
+	MazeSolver test1 = new MazeSolver("data2.txt");
+	MazeSolver test2 = new MazeSolver("data3.txt");
+	MazeSolver test3 = new MazeSolver("data4.txt");
+	MazeSolver test4 = new MazeSolver("data5.txt");
+	System.out.println(test);
+	test.solve(3);
+	System.out.println(test);
+	System.out.println(test1);
+	test1.solve(3);
+	System.out.println(test1);
+	System.out.println(test2);
+	test2.solve(3);
+	System.out.println(test2);
+	System.out.println(test3);
+	test3.solve(3);
+	System.out.println(test3);
+	System.out.println(test4);
+	test4.solve(3);
+	System.out.println(test4);
     }
 }
